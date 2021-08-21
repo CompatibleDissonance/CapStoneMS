3a0d9a959493245fc7f3dd3c6f6b3650b5cecdb0
Cian <cnlynch20@gmail.com>
2021-08-22 02:31:09 +1000

    Initial commit

---
+++ src/main/java/com/Cian/CapStoneMS/CapStoneMsApplication.java
@@ -0,0 +1,13 @@
+package com.Cian.CapStoneMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapStoneMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapStoneMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Cian/CapStoneMS/CapStoneMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cian.CapStoneMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapStoneMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
